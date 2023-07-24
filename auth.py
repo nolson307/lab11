4750add1c683050e10e95d997c7c0ce289b9fbee
nolson307 <s3732752@students.fscj.edu>
2023-07-23 18:39:48 -0700

    committing the lab 11 code

---
+++ auth.py
@@ -0,0 +1,18 @@
+import requests
+from getpass import getpass
+from requests.auth import HTTPBasicAuth
+
+username = input("please insert your username: ")
+password = getpass("please insert your password: ")
+
+url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
+payload={}
+headers = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': 'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
